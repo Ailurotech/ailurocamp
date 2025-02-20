cafa6c11f956887cbb035ae3f1aea824dcb71162
feixiang <913737079@qq.com>
2025-02-20 23:03:52 +1100

    add api to update a course

---
+++ src/app/api/course/[id]/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+import connectDB from "@/lib/mongodb";
+import Course from "@/models/Course";
+// import { Types } from "mongoose";
+
+export async function PATCH(req: Request, { params }: { params: { id: string } }) {
+  try {
+    const { id } = await params;
+    await connectDB();
+    const updatedContent = await req.json();
+    console.log("updatedContent", updatedContent);
+    
+    const updatedResult = await Course.findByIdAndUpdate(id, updatedContent, { new: true });
+    console.log("updatedResult", updatedResult);
+    
+    return NextResponse.json({ updatedResult });
+  } catch (error) {
+    console.error('Update course error:', error);
+    return NextResponse.json({ error: 'Error updating course', message: (error as Error).message }, { status: 500 });
+  }
+}
