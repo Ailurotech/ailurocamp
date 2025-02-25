37b6cc5f3716391f069cfa0f98f22cd1040aec81
feixiang <913737079@qq.com>
2025-02-25 21:24:23 +1100

    update models for course category and level

--- src/models/CourseLevel.ts
+++ src/models/CourseLevel.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const levelSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
+  level: { type: [String], required: true },
 });
 
 export default mongoose.models.Level || mongoose.model('Level', levelSchema);
