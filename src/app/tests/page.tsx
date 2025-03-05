c3f1df11fe8f582df4d18a938353d7e92af20b37
JackyChn <ccc1020048076@gmail.com>
2025-03-05 22:15:16 +1030

    Update: fetchAllProjects() test pass

--- src/app/tests/page.tsx
+++ src/app/tests/page.tsx
@@ -1,25 +1,12 @@
 'use client';
 
+import { fetchAllProjects } from '@/pages/api/githubApiServices/fetchAllProjects';
 import React, { useEffect } from 'react';
-import { fetchAllRepos } from '@/pages/api/fetchAllProjects';
-import { fetchProjectIssues } from '@/pages/api/fetchIssuesWithinProject';
-import {
-  addProjectCollaborator,
-  fetchProjectPermissions,
-} from '@/pages/api/manageProjectPermissions';
 
 export default function Tests() {
   useEffect(() => {
     // Change the test function here
-    FetchAllProjectsTest();
-    // FetchIssuesWithinProjectTest('ailurocamp');
-    // FetchProjectPermissionsTest('7', 'Ailurotech');
-    // addProjectCollaboratorTest({
-    //   // this project is not right, dont
-    //   projectId: '916372666',  // this is the aliproject_id is not correct
-    //   username: 'Ailurotech',
-    //   permission: 'write',
-    // });
+    // fetchAllProjects();
   }, []);
 
   return (
@@ -31,68 +18,3 @@ export default function Tests() {
     </>
   );
 }
-
-const FetchAllProjectsTest = () => {
-  const fetchData = async () => {
-    try {
-      const repos = await fetchAllRepos();
-      console.log('Fetched Repositories:', repos);
-    } catch (error) {
-      console.error('Error fetching repositories:', error);
-    }
-  };
-  fetchData();
-};
-
-const FetchIssuesWithinProjectTest = (repo: string) => {
-  const fetchData = async () => {
-    try {
-      const issues = await fetchProjectIssues(repo);
-      console.log('Fetched Issues:', issues);
-    } catch (error) {
-      console.error('Error fetching issues:', error);
-    }
-  };
-  fetchData();
-};
-
-const FetchProjectPermissionsTest = (projectId: string, username: string) => {
-  const fetchData = async () => {
-    try {
-      const permissions = await fetchProjectPermissions(projectId, username);
-      console.log(`Permissions for ${username}:`, permissions);
-    } catch (error) {
-      console.error('Error fetching project permissions:', error);
-    }
-  };
-  fetchData();
-};
-
-const addProjectCollaboratorTest = ({
-  projectId,
-  username,
-  permission = 'write',
-}: {
-  projectId: string;
-  username: string;
-  permission?: 'admin' | 'write' | 'read';
-}) => {
-  const fetchData = async () => {
-    try {
-      const response = await addProjectCollaborator(
-        projectId,
-        username,
-        permission as 'admin' | 'write' | 'read'
-      );
-
-      console.log(
-        `Collaborator ${username} added with ${permission} permission:`,
-        response
-      );
-    } catch (error) {
-      console.error('Error adding project collaborator:', error);
-    }
-  };
-
-  fetchData();
-};
---
+++ src/pages/api/githubApiServices/fetchAllProjects.ts
@@ -0,0 +1,44 @@
+const GITHUB_GRAPHQL_API = 'https://api.github.com/graphql';
+const GITHUB_TOKEN = process.env.NEXT_PUBLIC_GITHUB_TOKEN;
+
+export function fetchAllProjectsQuery() {
+  return `
+  {
+    organization(login: "Ailurotech") {
+      projectsV2(first: 100) {
+        nodes {
+          id
+          title
+          url
+        }
+      }
+    }
+  }
+  `;
+}
+
+export async function fetchAllProjects() {
+  try {
+    const response = await fetch(GITHUB_GRAPHQL_API, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${GITHUB_TOKEN}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ query: fetchAllProjectsQuery() }),
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      console.log('data: ', data.data.organization.projectsV2.nodes);
+      return data.data.organization.projectsV2.nodes;
+    } else {
+      console.error('Error fetching projects:', data);
+      return null;
+    }
+  } catch (error) {
+    console.error('Fetch error:', error);
+    return null;
+  }
+}
