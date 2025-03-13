f115e632ed27321cc8bf9fb5d31af59d174c0b1c
JackyChn <ccc1020048076@gmail.com>
2025-03-14 07:43:02 +1030

    Update: getStatusColumn

--- src/app/tests/page.tsx
+++ src/app/tests/page.tsx
@@ -1,18 +1,15 @@
 'use client';
+import {
+  checkRateLimit,
+  fetchAllProjects,
+  fetchIssuesWithinProjects,
+} from '@/services/github';
 import React, { useEffect } from 'react';
-
-import { fetchAllProjects } from '@/pages/api/githubApiServices/fetchAllProjects';
-import { fetchIssuesWithinProjects } from '@/pages/api/githubApiServices/fetchIssuesWithinProject';
-import { getStatusColumn } from '@/pages/api/githubApiServices/projectColumnManagement/getStatusColumn';
-import { checkRateLimit } from '@/pages/api/githubApiServices/checkApiRateLimiting';
-import { listProjectColumns } from '@/pages/api/githubApiServices/projectColumnManagement/listProjectColumns';
-import { updateStatusColumn } from '@/pages/api/githubApiServices/projectColumnManagement/updateStatusColumn';
-import { addIssueToProject } from '@/pages/api/githubApiServices/projectTicketManagement/addTicketToProject';
 
 export default function Tests() {
   // Change the test function here
   useEffect(() => {
-    // fetchAllProjects();
+    fetchAllProjects();
     // fetchIssuesWithinProjects('ailurocamp');
     // checkRateLimit();
     // listProjectColumns('PVT_kwDOCWE1Yc4AwAI5'); // this is the project_id for 'ailurocamp'
--- src/services/github.ts
+++ src/services/github.ts
@@ -858,3 +858,50 @@ export async function fetchIssuesWithinProjects(repo: string) {
     return null;
   }
 }
+
+export async function getStatusColumn(columnId: string) {
+  const query = `
+    query GetProjectColumn($columnId: ID!) {
+      node(id: $columnId) {
+        ... on ProjectV2SingleSelectField {
+          id
+          name
+          dataType
+          options {
+            id
+            name
+          }
+        }
+      }
+    }
+  `;
+  try {
+    const response = await fetch(GITHUB_GRAPHQL_API, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${GITHUB_TOKEN}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: query,
+        variables: { columnId },
+      }),
+    });
+
+    const data = await response.json();
+
+    console.log('GraphQL Response:', JSON.stringify(data, null, 2));
+
+    if (!data?.data?.node) {
+      console.error("Error: Missing 'node' in API response. Check column ID.");
+      console.error('Full Response:', data);
+      return null;
+    }
+
+    console.log('Project Column:', data.data.node);
+    return data.data.node;
+  } catch (error) {
+    console.error('Fetch error:', error);
+    return null;
+  }
+}
